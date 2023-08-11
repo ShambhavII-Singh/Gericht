b1bbfc5b128a3ab8783d0bc54d5d9abde54dfc94
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-08-12 03:05:01 +0530

    made more sections

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className='app__menuItem'>
+    <div className='menuItem__heading'>
+      <div className='menuItem__heading-title'>
+        <p className='p__cormorant' style={{ color: '#DCCA87' }}>
+          { title }
+        </p>
+      </div>
+      <div className='menuItem__heading-dash' />
+      <div className='menuItem__heading-price'>
+        <p className='p__cormorant' style={{ color: '#DCCA87' }}>
+          { price }
+        </p>
+      </div>
+    </div>
+    <div className='menuItem__subtitle'>
+      <p className="p__opensans" style={{ color: '#AAAAAA' }}>
+        { tags }
+      </p>
+    </div>
   </div>
 );
 
